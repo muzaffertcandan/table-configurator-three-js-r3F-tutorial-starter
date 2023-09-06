bf3e76acda9af82701b7dcd00609f7e4b5b12a2c
Muzaffer Talha Candan <muzaffertcandan@gmail.com>
2023-09-06 17:21:03 +0300

    hey

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,29 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stage } from "@react-three/drei";
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      <Stage
+        intensity={1.5}
+        environment="city"
+        shadows={{
+          type: "accumulative",
+          color: "#d9afd9",
+          colorBlend: 2,
+          opacity: 2
+        }}
+        adjustCamera={0.9}>
+          
+        <mesh castShadow>
+          <meshNormalMaterial />
+          <boxGeometry />
+        </mesh>
+      </Stage>
+      <OrbitControls
+        makeDefault
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI / 2}
+      />
     </>
   );
 };
